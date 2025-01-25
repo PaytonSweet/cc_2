ded342fd87771675f67af5151b6ea1709383fe23
Payton <paytonsweet@usf.edu>
2025-01-25 11:50:32 -0500

    Task 1 - Store Inventory.

---
+++ cc_2.js
@@ -0,0 +1,6 @@
+// Task 1: Working with Arrays
+
+let products = ["Apple", "Banana", "Coconut", "Dragonfruit", "Pear"];
+products.unshift("Watermelon");
+let selected = products.slice(0,5);
+console.log(selected);
