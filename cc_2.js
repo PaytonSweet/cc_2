34312fdd67db9e877d1827a3517de7b5d1dc2339
Payton <paytonsweet@usf.edu>
2025-01-25 12:04:19 -0500

    Task 2 - Student Scores.

--- cc_2.js
+++ cc_2.js
@@ -4,3 +4,16 @@ let products = ["Apple", "Banana", "Coconut", "Dragonfruit", "Pear"];
 products.unshift("Watermelon");
 let selected = products.slice(0,5);
 console.log(selected);
+
+// Task 2: Accessing and Modifying Arrays
+
+let scores = [2, 4, 6, 8, 10];
+scores[1] = 10;
+let total = scores.reduce((total, amount) => total + amount, 0);
+let average = total / scores.length;
+
+console.log(scores);
+console.log(average);
+
+// Task 3: Working with Objects
+
